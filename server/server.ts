46af58c51cd7c9b9ed6b624da611a5d2fea1ce6b
SanidhyaGupta-10 <adityasgupta1857@gmail.com>
2026-03-18 17:34:16 +0530

    feat: Initialize server with Express, Prisma, and basic User and Question database models.

--- server/server.ts
+++ server/server.ts
@@ -1,8 +1,10 @@
 import app from './src/app';
 import 'dotenv/config';
+import { connectDB } from './src/config/db';
 
 const PORT = process.env.PORT
 
+connectDB()
 app.listen(PORT, () => {
   console.log(`🚀 Server ready at http://localhost:${PORT}`);
 });
