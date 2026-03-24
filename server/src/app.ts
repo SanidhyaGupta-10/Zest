e899e58d6e62e89154fe974b13eaff9e5f1a6176
SanidhyaGupta-10 <adityasgupta1857@gmail.com>
2026-03-24 12:52:36 +0530

    PORT

--- server/src/app.ts
+++ server/src/app.ts
@@ -9,7 +9,7 @@ const app = express();
 app.set('strict routing', false);
 
 app.use(cors({
-  origin: "http://localhost:3000",
+  origin: "https://zest-delta.vercel.app",
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"]
